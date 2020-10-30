f4a71076b144885abe69896c9169b6bd97685c9c
Victorse9 <victor.rse97@gmail.com>
2020-10-30 12:10:02 +0100

    He conseguido hacer un commit usando GIT

---
+++ Victor_R - copia/src/app/app-routing.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { InicioPageModule } from './pages/inicio/inicio.module';
+
+const routes: Routes = [
+  {
+    path: 'inicio',
+    loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'inicio',
+    pathMatch: 'full'
+  },
+  {
+    path: 'datos-personales',
+    loadChildren: () => import('./pages/datos-personales/datos-personales.module').then( m => m.DatosPersonalesPageModule)
+  },
+  {
+    path: 'cursos',
+    loadChildren: () => import('./pages/cursos/cursos.module').then( m => m.CursosPageModule)
+  },
+  {
+    path: 'dam1',
+    loadChildren: () => import('./pages/dam1/dam1.module').then( m => m.Dam1PageModule)
+  },
+  {
+    path: 'dam2',
+    loadChildren: () => import('./pages/dam2/dam2.module').then( m => m.Dam2PageModule)
+  },
+  {
+    path: 'fct',
+    loadChildren: () => import('./pages/fct/fct.module').then( m => m.FctPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
